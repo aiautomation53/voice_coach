e3956d6ffc988ff5567f1d0f5d167adf2b0971d2
burhan <burhan.kutub@gmail.com>
2025-08-28 17:07:33 +0530

    fix: Wrap Vapi widget in div for custom positioning

--- src/pages/HospitalityCoach.tsx
+++ src/pages/HospitalityCoach.tsx
@@ -186,7 +186,8 @@ const HospitalityCoach = () => {
         
         
         <div className="flex justify-start pl-0">
-            <vapi-widget style={{ position: 'fixed', top: '100px', left: '100px' }}              public-key="20234fe3-f02a-4cd7-9cf3-addcf5935775"
+            <div style={{ position: 'fixed', top: '100px', left: '100px' }}>
+              <vapi-widget              public-key="20234fe3-f02a-4cd7-9cf3-addcf5935775"
               assistant-id="5fe73bc9-a36a-4bdc-b6d2-e84bc3874497"
               mode="voice"
               theme="dark"
@@ -206,6 +207,7 @@ const HospitalityCoach = () => {
               consent-title="Terms and conditions"
               consent-storage-key="vapi_widget_consent"
             ></vapi-widget>
+            </div>
           </div>
         </div>
       </div>
